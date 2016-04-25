9c18512db3ad20bd980a1a4fbbcc7b44a8f2064d
Charles Zheng <snarles@stanford.edu>
2016-04-25 10:07:53 -0700

    solving 25 adj 70s

--- benchmarks/adjacent.R
+++ benchmarks/adjacent.R
@@ -7,10 +7,26 @@
 library(Rcpp)
 sourceCpp("benchmarks/adjacent1.cpp")
 t1 <- proc.time()
-moveValues(rep(0, 18), 10)
+moveValues(rep(0, 20), 10)
 proc.time() - t1
 
 sourceCpp("benchmarks/adjacent2.cpp")
 t1 <- proc.time()
-moveValues(rep(0, 18), 10)
+moveValues(rep(0, 20), 10)
+proc.time() - t1
+
+t1 <- proc.time()
+moveValues(rep(0, 22), 22)
+proc.time() - t1
+
+t1 <- proc.time()
+moveValues(rep(0, 23), 23)
+proc.time() - t1
+
+t1 <- proc.time()
+moveValues(rep(0, 24), 24)
+proc.time() - t1
+
+t1 <- proc.time()
+moveValues(rep(0, 25), 25)
 proc.time() - t1
