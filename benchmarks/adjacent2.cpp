df446e8d5b7897f46a072e48428d59da1b1560a2
Charles Zheng <snarles@stanford.edu>
2016-04-25 08:21:12 -0700

    found bug, need to fix

--- benchmarks/adjacent2.cpp
+++ benchmarks/adjacent2.cpp
@@ -9,34 +9,38 @@ using namespace Rcpp;
 int makeMove(IntegerVector board, int n, int depth, int loc) {
   // check for loss
   if (board[loc] != 0) {
+    Rprintf("|occ|");
     return -1;
   }
   if (loc == 0 && board[1] != 0) {
+    Rprintf("|0a|");
     return -1;
   }
   if (loc == (n - 1) && board[(n - 2)] != 0) {
+    Rprintf("|na|");
     return -1;
   }
   if (board[loc + 1] != 0 || board[loc - 1] != 0) {
+    Rprintf("|adjc|");
     return -1;
   }
   if (depth == 0) {
+    Rprintf("|ret0|");
     return 0;
   }
-  else {
-    board[loc] = 1;
-    int ans = 1;
-    int i = 0;
-    while (i < n && ans == 0) {
-      int newans = -makeMove(board, n, depth - 1, i);
-      if (newans < ans) {
-        ans = newans;
-      }
+  Rprintf("DADA");
+  board[loc] = 1;
+  int ans = 1;
+  int i = 0;
+  while (i < n && ans == 0) {
+    int newans = -makeMove(board, n, depth - 1, i);
+    if (newans < ans) {
+      ans = newans;
     }
-    // undo move!
-    board[loc] = 0;
-    return(ans);
   }
+  // undo move!
+  board[loc] = 0;
+  return(ans);
 }
 
 // [[Rcpp::export]]
