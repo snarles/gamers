1bfe0fc5c8dd1440124666f36c9ae28c4d670fc1
Charles Zheng <snarles@stanford.edu>
2016-04-25 19:03:56 -0700

    astuff

--- doubutsu1/lg_analysis_setup.R
+++ doubutsu1/lg_analysis_setup.R
@@ -3,7 +3,7 @@
 ####
 
 source("doubutsu1/source.R")
-sourceCpp("doubutsu1/Rsource2.cpp")
+# sourceCpp("doubutsu1/Rsource2.cpp")
 hashtab <- readRDS("doubutsu1/hashtab.rds")
 matein <- readRDS("doubutsu1/matein.rds")
 
@@ -14,23 +14,3 @@ saveRDS(hashes, "doubutsu1/lg_hashes.rds")
 load("doubutsu1/lg_data.rda", verbose = TRUE)
 length(games)
 length(glist)
-
-save()
-
-ginds <- which(sapply(glist, length) > 2)
-(gno <- sample(ginds, 1))
-(game <- glist[[gno]])
-mi <- matein[hashes[[gno]]]
-names(mi) <- NULL
-if (rev(game)[1] == "resign") mi <- c(mi, Inf)
-cbind(game, mi)
-
-gametable[gno, ]
-
-
-####
-##  Player stats?
-####
-
-sum(gametable$sente == gametable$loser)
-sum(gametable$sente == gametable$winner)
