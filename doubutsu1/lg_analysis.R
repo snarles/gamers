d4c12c6872e88870f6659a0fc6b1fd97e832fe3e
Charles Zheng <snarles@stanford.edu>
2016-04-25 19:01:32 -0700

    just save teh hashes

--- doubutsu1/lg_analysis.R
+++ doubutsu1/lg_analysis.R
@@ -1,9 +1,11 @@
 source("doubutsu1/source.R")
+sourceCpp("doubutsu1/Rsource2.cpp")
 hashtab <- readRDS("doubutsu1/hashtab.rds")
 matein <- readRDS("doubutsu1/matein.rds")
 
 games <- readRDS("doubutsu1/lg_states.rds")
 hashes <- lapply(games, function(v) sapply(v, hash_state))
+saveRDS(hashes, "doubutsu1/lg_hashes.rds")
 
 load("doubutsu1/lg_data.rda", verbose = TRUE)
 length(games)
@@ -19,3 +21,11 @@ if (rev(game)[1] == "resign") mi <- c(mi, Inf)
 cbind(game, mi)
 
 gametable[gno, ]
+
+
+####
+##  Player stats?
+####
+
+sum(gametable$sente == gametable$loser)
+sum(gametable$sente == gametable$winner)
