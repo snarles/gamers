215c2811b3aab32763869e259609f935a7de3c9d
Charles Zheng <snarles@stanford.edu>
2016-04-25 19:23:43 -0700

    move prediction data

--- doubutsu1/feature_ex.R
+++ doubutsu1/feature_ex.R
@@ -6,7 +6,7 @@ source("doubutsu1/source.R")
 sourceCpp("doubutsu1/Rsource.cpp")
 games <- readRDS("doubutsu1/lg_states.rds")
 
-itree <- build_tree(init_state, 1, 100)[, -1]
+itree <- build_tree(init_state, 1, 100)[-1, ]
 
 games2 <- lapply(games, function(v) {
   if (is.null(v)) {
@@ -15,5 +15,21 @@ games2 <- lapply(games, function(v) {
   ff <- function(x) build_tree(x, depth = 1, nodemax = 100)[-1, , drop = FALSE]
   c(list(itree), lapply(v, ff))
 })
+
+for (i in 1:length(games)) {
+  if (length(games[[i]]) > 0) {
+    for (j in 1:length(games[[i]])) {
+      chosen <- games[[i]][[j]]
+      alt <- games2[[i]][[j]]
+      ind <- prodlim::row.match(chosen[-(1:2)], alt[, -(1:2)])
+      if (is.na(ind)) {
+        print(c(i, j))
+      }
+      alt[ind, 1] <- 1
+      games2[[i]][[j]] <- alt
+    }
+  }
+}
+
 
 saveRDS(games2, "doubutsu1/altMoves.rds")
