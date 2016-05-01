f35762b8d97a05d69aadb98a50452f7453d90fd1
snarles <snarles@stanford.edu>
2016-05-01 16:28:03 -0700

    prepare for comparative analysis

--- doubutsu1/prediction/multinom_fit2_byrank.R
+++ doubutsu1/prediction/multinom_fit2_byrank.R
@@ -9,6 +9,9 @@ tab <- readRDS("doubutsu1/prediction/player_ranks.rds")
 players <- rownames(tab)
 (player <- players[order(-tab$total)[5]])
 
+(select <- players[tab$rating > 0.2 & tab$total > 5])
+#(select <- players[tab$rating < -0.2 & tab$total > 5])
+
 mats <- resTr$senteAlts
 choice <- resTr$senteChoice
 pl <- resTr$sentePl
@@ -17,15 +20,23 @@ pl <- resTr$sentePl
 
 wtrange <- seq(0, 10, 0.25)
 accs <- 0 * wtrange
-filt <- which(resTe$sentePl == player)
+filt <- which(resTe$sentePl %in% select)
 
 for (i in 1:length(wtrange)) {
   bt <- readRDS("doubutsu1/prediction/multinom_fit2_sente.rds")
-  weights <- 1 + wtrange[i] * (pl == player)
+  weights <- 1 + wtrange[i] * (pl %in% select)
   weights <- weights/sum(weights) * length(weights)
   bt <- mcm_sgd(mats, choice, bt, l1p = 2e-5, l2p = 1e-5, eps = 0.01, weights = weights)
   res <- mcm_loss(resTe$senteAlts[filt], resTe$senteChoice[filt], resTe$senteMoves[filt], bt)
   accs[i] <- res[[1]]
 }
 plot(wtrange, accs, type = "l")
-title(player)
+title("Top players")
+# title("Least-rated players")
+
+# bt <- readRDS("doubutsu1/prediction/multinom_fit2_sente.rds")
+# weights <- 1 + 2 * (pl %in% select)
+# weights <- weights/sum(weights) * length(weights)
+# bt <- mcm_sgd(mats, choice, bt, l1p = 2e-5, l2p = 1e-5, eps = 0.01, weights = weights)
+# 
+# saveRDS(bt, "doubutsu1/prediction/multinom_fit2_sente_topPL.rds")
