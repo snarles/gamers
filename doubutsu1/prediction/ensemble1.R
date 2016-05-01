df5978c669bef3b5271f1b52c10b4d32cc13c916
snarles <snarles@stanford.edu>
2016-05-01 16:00:46 -0700

    ranking players

--- doubutsu1/prediction/ensemble1.R
+++ doubutsu1/prediction/ensemble1.R
@@ -4,38 +4,38 @@ source("doubutsu1/prediction/spolicy.R")
 source("doubutsu1/prediction/multinom2.R")
 source("doubutsu1/prediction/multiclassreg.R")
 
+## SENTE PREDICTION
+
 bt1 <- readRDS("doubutsu1/prediction/multinom_fit2_sente.rds")
 res_ev_st <- mcm_loss(resTe$senteAlts, resTe$senteChoice, resTe$senteMoves, bt1)
-res_ev_st[1]
-
-bt2 <- readRDS("doubutsu1/prediction/multinom_fit2_gote.rds")
-res_ev_gt <- mcm_loss(resTe$goteAlts, resTe$goteChoice, resTe$goteMoves, bt2)
-res_ev_gt[1]
+res_ev_st[1] # 0.566
 
 B1 <- readRDS("doubutsu1/prediction/spfit1_sente.rds")
 Xte <- resTe$senteX
 Yte <- match(resTe$senteChosen, allmoves)
 Ycte <- lapply(resTe$senteMoves, function(v) match(v, allmoves))
 res_sp_st <- predict_acc(Xte, B1, Yte, Ycte)
-res_sp_st[1]
+res_sp_st[1] # 0.563
+
+pmat_sp <- .5 * res_ev_st$pmat + .5 * res_sp_st$pmat
+
+pred_sp <- legal_preds(pmat_sp, resTe$senteMoves)
+sum(pred_sp==resTe$senteChosen)/length(resTe$senteChosen) # 0.611
+
+## GOTE PREDICTION
+
+bt2 <- readRDS("doubutsu1/prediction/multinom_fit2_gote.rds")
+res_ev_gt <- mcm_loss(resTe$goteAlts, resTe$goteChoice, resTe$goteMoves, bt2)
+res_ev_gt[1] # 0.579
 
 B2 <- readRDS("doubutsu1/prediction/spfit1_gote.rds")
 Xte <- resTe$goteX
 Yte <- match(resTe$goteChosen, allmoves)
 Ycte <- lapply(resTe$goteMoves, function(v) match(v, allmoves))
 res_sp_gt <- predict_acc(Xte, B2, Yte, Ycte)
-res_sp_gt[1]
+res_sp_gt[1] # 0.547
 
-dim(res_ev_st$pmat)
-dim(res_ev_gt$pmat)
-dim(res_sp_st$pmat)
-dim(res_sp_gt$pmat)
-
-pmat_sp <- .5 * res_ev_st$pmat + .5 * res_sp_st$pmat
 pmat_gt <- .5 * res_ev_gt$pmat + .5 * res_sp_gt$pmat
-
-pred_sp <- legal_preds(pmat_sp, resTe$senteMoves)
-sum(pred_sp==resTe$senteChosen)/length(resTe$senteChosen) # 0.611
 
 pred_gt <- legal_preds(pmat_gt, resTe$goteMoves)
 sum(pred_gt==resTe$goteChosen)/length(resTe$goteChosen) # 0.585
---
+++ doubutsu1/prediction/player_ranking.R
@@ -0,0 +1,56 @@
+
+load("doubutsu1/lg_data.rda", verbose = TRUE)
+players <- sort(unique(c(gametable$sente, gametable$gote)))
+
+filt <- (gametable$len > 2) & (gametable$winner != "")
+
+n <- sum(filt)
+gt <- gametable[filt, ]
+p <- length(players)
+
+X <- matrix(0, n, p)
+colnames(X) <- players
+y <- (gt$sente == gt$winner) + 0
+for (i in 1:n) {
+  X[i, gt$sente[i]] <- 1
+  X[i, gt$gote[i]] <- -1
+}
+
+library(glmnet)
+res <- glmnet(X, y, alpha = 0)
+cf0 <- coef(res, s = 0)[-1]
+names(cf0) <- players
+wins <- sapply(players, function(v) sum(gt$winner == v))
+losses <- sapply(players, function(v) sum(gt$loser == v))
+tab <- data.frame(rating = cf0, wins, losses, total = wins + losses)
+View(tab[order(-cf0), ])
+
+## bootstrap coefficients
+mc.reps <- 100
+cfs <- matrix(0, mc.reps, p)
+for (i in 1:mc.reps) {
+  inds <- sample(n, n, TRUE)
+  res <- glmnet(X[inds, ], y[inds], alpha = 0)
+  cfs[i, ] <- coef(res, s = 0)[-1]
+}
+xl <- c(-1.5, 1.0)
+boxplot(cfs[, 1:23], horizontal = TRUE, names = paste(1:23), ylim = xl)
+boxplot(cfs[, 24:46], horizontal = TRUE, names = paste(24:46), ylim = xl)
+boxplot(cfs[, 47:69], horizontal = TRUE, names = paste(47:69), ylim = xl)
+
+boxplot(cfs, horizontal = TRUE, ylim = xl)
+points(cf0, 1:p, pch = "+", col = "red")
+
+o <- order(cf0)
+boxplot(cfs[, o], horizontal = TRUE, ylim = xl)
+points(cf0[o], (1:p), pch = "+", col = "red")
+
+sort(cf0, decreasing = TRUE)
+
+
+## filter by total
+filt <- tab$total > 10
+o <- order(cf0[filt])
+boxplot(cfs[, filt][, o], horizontal = TRUE, ylim = xl)
+points(cf0[filt][o], 1:sum(filt), pch = "+", col = "red")
+
