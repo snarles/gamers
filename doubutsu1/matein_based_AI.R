41e58d6d730ca9d02e3e251574a4b4fc3eb69274
Charles Zheng <snarles@stanford.edu>
2016-05-24 12:34:39 -0700

    minsp

--- doubutsu1/matein_based_AI.R
+++ doubutsu1/matein_based_AI.R
@@ -1,9 +1,16 @@
-mseekS <- 20
-mavoidS <- 20
-mseekG <- 20
-mavoidG <- 20
+val_in_env <- function(var, def) {
+  if (var %in% ls()) {
+    return(.GlobalEnv[[var]])
+  }
+  def
+}
 
-next_move <- function(state) {
+mseekS <- val_in_env("mseekS", 20)
+mavoidS <- val_in_env("mavoidS", 20)
+mseekG <- val_in_env("mseekG", 20)
+mavoidG <- val_in_env("mavoidG", 20)
+
+next_move <- function(state, mseekS = mseekS, mavoidS = mavoidS, mseekG = mseekG, mavoidG = mavoidG) {
   #raw <- solve_state_raw(state, TRUE)
   if (state[4] %% 2 == 1) {
     mseek <- mseekG; mavoid <- mavoidG
@@ -30,4 +37,44 @@ next_move <- function(state) {
   }
   ind <- order(-mn[, 2])[1]
   return(rownames(mn)[ind])
+}
+
+## resigns if less than seek value
+next_move2 <- function(state, mseekS = mseekS, mavoidS = mavoidS, mseekG = mseekG, mavoidG = mavoidG) {
+  #raw <- solve_state_raw(state, TRUE)
+  if (state[4] %% 2 == 1) {
+    mseek <- mseekG; mavoid <- mavoidG
+  } else {
+    mseek <- mseekS; mavoid <- mavoidS
+  }
+  alt <- getAlt(state)
+  raw <- solve_state_raw(state)
+  res <- analysis_to_values(raw)
+  alt <- res[[1]]
+  mn <- alt[-1, 1:2, drop = FALSE]
+  if (state[4] %% 2 == 1) mn[, 1] <- -mn[, 1]
+  filt1 <- mn[, 1]==1 & mn[, 2] < mseek
+  if (sum(filt1) > 0) {
+    mvs <- rownames(mn)[filt1]
+    print(mn[filt1, , drop = FALSE])
+    return(sample(mvs, 1))
+  }
+  filt2 <- (mn[, 1]==0) | (mn[, 1]==1) | (mn[, 1]==-1 & mn[, 2] > mavoid)
+  if (sum(filt2) > 0) {
+    mvs <- rownames(mn)[filt2]
+    print(mn[filt2, , drop = FALSE])
+    return(sample(mvs, 1))
+  }
+  return("resign")
+}
+
+get_sp <- function(state = init_state, seek_val = 20) {
+  mv <- ""
+  game <- character()
+  while (mv != "resign") {
+    mv <- next_move2(state, seek_val, seek_val, seek_val, seek_val)
+    game <- c(game, mv)
+    state <- move_parser(state, mv)
+  }
+  return(game)
 }
